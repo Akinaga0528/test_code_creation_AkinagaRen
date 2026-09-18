00df6276a147b880ea6442f56f6c01d6ec854b40
r-akinaga <r-akinaga@NDSSYS79.ad.ndsc.co.jp>
2026-09-18 15:39:09 +0900

    fix:秋永
    ログイン失敗時のエラーメッセージのチェックテストを追加しました。
    期待値：* ログインに失敗しました。のメッセージが表示される

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,48 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページにアクセス
+		goTo("http://localhost:8080/lms/");
+
+		//ログイン画面の検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// トップページ（ログイン画面）へアクセス
+		goTo("http://localhost:8080/lms/");
+
+		String testUser = "test";
+		String testPassword = "test123";
+
+		// ログインID入力
+		WebElement inputLoginId = webDriver.findElement(By.id("loginId"));
+		inputLoginId.clear();
+		inputLoginId.sendKeys(testUser);
+
+		// パスワード入力
+		WebElement password = webDriver.findElement(By.id("password"));
+		password.clear();
+		password.sendKeys(testPassword);
+
+		// ログインボタン押下
+		WebElement loginBtn = webDriver.findElement(By.className("btn-primary"));
+		loginBtn.click();
+
+		// エラーメッセージチェック		
+		WebElement errorMsg = webDriver.findElement(By.className("help-inline"));
+		assertEquals("* ログインに失敗しました。", errorMsg.getText());
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
